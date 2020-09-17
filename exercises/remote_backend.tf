8e9a8df09411828aa2335633dc4bdf180491d3b0
Sacha Wharton <sacha@gmail.com>
2020-09-17 20:56:16 +0200

    updated organisation

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "RavenBlack"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-aws"
     }
   }
 }
